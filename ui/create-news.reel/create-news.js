2c6fdd0cf8610564ddbb4707811918adbd10f921
Michel Mongkhoy <noreply>
2014-05-15 20:10:54 +0000

    Add component create-news

---
+++ ui/create-news.reel/create-news.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/create-news.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class CreateNews
+ * @extends Component
+ */
+exports.CreateNews = Component.specialize(/** @lends CreateNews# */ {
+    constructor: {
+        value: function CreateNews() {
+            this.super();
+        }
+    }
+});
